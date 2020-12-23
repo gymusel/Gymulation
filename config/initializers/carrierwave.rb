e7777b4306bfee4ac64362c3320b28a3650aa7e8
Shigeki Yamaguchi <shigeki@Shigekis-MBP.chofu1.kn.home.ne.jp>
2020-12-23 17:37:02 +0900

    precompile2

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -8,9 +8,9 @@ CarrierWave.configure do |config|
   config.fog_directory  = 'gymulation'
   config.fog_credentials = {
     provider: 'AWS',
-    aws_access_key_id: ENV['AKIA3L3EX745LIW4VRVB'],
-    aws_secret_access_key: ENV['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'],
-    region: ENV['ap-northeast-1'],
+    aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
+    aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
+    region: ENV['AWS_DEFAULT_REGION'],
     path_style: true
   }
 
